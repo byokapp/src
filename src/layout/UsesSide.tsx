b13299c882fa818ef15c716ac2ee12bd8fcd5285
artlu99 <alu99.sm@gmail.com>
2023-06-18 16:37:35 -0400

    clean up imports

--- src/layout/UsesSide.tsx
+++ src/layout/UsesSide.tsx
@@ -1,18 +1,18 @@
 import { FunctionComponent } from 'preact';
 import { Link } from 'wouter-preact';
-import { Divider, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 
 const UsesSide: FunctionComponent = () => {
   return (
-      <List dense={false}>
-        <ListItem key={'Home'} disablePadding>
-          <ListItemButton>
-            <Link href="/">
-              <ListItemText primary={'Home'} />
-            </Link>
-          </ListItemButton>
-        </ListItem>
-      </List>
+    <List dense={false}>
+      <ListItem key={'Home'} disablePadding>
+        <ListItemButton>
+          <Link href="/">
+            <ListItemText primary={'Home'} />
+          </Link>
+        </ListItemButton>
+      </ListItem>
+    </List>
   );
 };
 
